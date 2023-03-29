045447393fdeb6fe2edbf921a57d684dd1355e02
Rishi <rishi@192.168.1.4>
2023-03-29 21:22:35 +0530

    added new notes page and routes

---
+++ src/NewNote.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NoteData, Tag } from "./App";
+import NoteForm from "./NoteForm";
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+const NewNote = ({ onSubmit, onAddTag, availableTags }: NewNoteProps) => {
+  return (
+    <>
+      <h1 className="mb-4">NewNote</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+};
+
+export default NewNote;
